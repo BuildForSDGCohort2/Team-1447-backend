86f25303b529a0bd8cbfb4882a7e2332af0b1715
FuFuWarrior <akpuwarrior@gmail.com>
2020-09-12 12:12:29 +0100

    made changes

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ const limiter = rateLimit({
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 // to make sure only your domain is the only one that has access
-// const isProduction = process.env.NODE_ENV === "production"
-// const origin = {
-//   origin: isProduction ? "https://www.devstory.netlify.app" : "*",
-// }
+const isProduction = process.env.NODE_ENV === "production"
+const origin = {
+  origin: isProduction ? "https://www.devstory.netlify.app" : "*",
+}
 
 // setup bodyParser
 app.use(express.json());
@@ -45,13 +45,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(helmet());
 app.use(compression());
 app.use(limiter); // This is applicable to all routes
-app.use(cors());
+app.use(cors(origin));
 
 app.use(morgan(':method   :url    :status     is done in    :response-time ms', {stream: accessLogStream}));
 
 // homepage
 app.get("/", (req, res) => {
-    res.status(200).json("Welcome to devstories");
+    res.status(200).json( "Welcome to devstories" );
 });
 
 // setup the routes
