341cd6b282357446d59bdbbd24fba7c663a116a1
FuFuWarrior <akpuwarrior@gmail.com>
2021-06-04 03:01:32 +0100

    deploy

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,17 @@
 const {Pool} = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 //connection config for production
+// const pool = new Pool({
+//     connectionString: isProduction ? process.env.DATABASE_URL : process.env.HEROKU_POSTGRESQL_GREEN_URL,
+//     ssl: true
+// });
+
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : process.env.HEROKU_POSTGRESQL_GREEN_URL,
-    ssl: true
+   ssl: {
+    rejectUnauthorized: false
+  },
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
