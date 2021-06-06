b72d379f125ddb4b55f4be202b5aae6f18380bfa
FuFuWarrior <akpuwarrior@gmail.com>
2021-06-06 20:08:56 +0100

    made changes to app.js

--- app.js
+++ app.js
@@ -13,30 +13,31 @@ const media = require("./src/routes/mediaRoute");
 const comment = require("./src/routes/commentRoute");
 const search = require("./src/routes/searchRoute");
 
-// intialize express
+// initialize express
 const app = express();
 
 app.disable("x-powered-by");
 
 // this will be used to make rate limit on a particular route, it should be place before the jwt on the post routes
-// const postLimiter = rateLimit({
-//     windowMs: 1 * 60 * 1000,
-//     max: 1,
-//   });
+const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 1,
+  });
+
 // This is a limiter for the whole request
-// const limiter = rateLimit({
-//     windowMs: 1 * 60 * 1000, // 1 minute
-//     max: 5, // 5 requests,
-// });
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 5, // 5 requests,
+});
 
 // Creates a writeable stream to the file instead of process.stdout
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 // to make sure only your domain is the only one that has access
-// const isProduction = process.env.NODE_ENV === "production"
-// const origin = {
-//   origin: isProduction ? "https://devssstory.netlify.app" : "*",
-// }
+const isProduction = process.env.NODE_ENV === "production"
+const origin = {
+  origin: isProduction ? "https://devssstory.netlify.app" : "*",
+}
 
 // setup bodyParser
 app.use(express.json());
@@ -45,7 +46,7 @@ app.use(express.urlencoded({extended: false}));
 // app.use(helmet());
 app.use(compression());
 // app.use(limiter); // This is applicable to all routes
-app.use(cors());
+app.use(cors(origin));
 
 app.use(morgan(':method   :url    :status     is done in    :response-time ms', {stream: accessLogStream}));
 
